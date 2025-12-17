d25eebca16ffe3782c37306dbff823f79b969fb5
ChepteneNicolae <117681034+nikotsvetkoff@users.noreply.github.com>
2025-12-17 22:30:06 +0200

    Refactor EPG collector for timezone adjustments

--- epg_collector.php
+++ epg_collector.php
@@ -1,70 +1,61 @@
 <?php
-// epg_collector.php – varianta finală
+// epg_timezone.php – ajustare fus orar
 ini_set('memory_limit', '512M');
 ini_set('max_execution_time', '300');
 date_default_timezone_set("Europe/Chisinau");
 
-$sources = [
-    "compress.zlib://http://epg.it999.ru/epg.xml.gz"
-];
+// sursa EPG
+$source = "compress.zlib://http://epg.it999.ru/epg.xml.gz";
 
-$channels = file("channels.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$channels = array_map(function($line) {
-    $id = trim(strtok($line, "#"));
-    return strtolower($id);
-}, $channels);
+// fus orar dorit
+$targetTZ = new DateTimeZone("Europe/Chisinau");
 
 // deschide fișierul comprimat pentru scriere
-$out = gzopen("epg.xml.gz", "w9"); // nivel maxim de compresie
+$out = gzopen("epg_fixed.xml.gz", "w9");
 gzwrite($out, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<tv>\n");
 
-function fetchEPG($url, $channels, $out) {
-    $reader = new XMLReader();
-    if (!$reader->open($url)) {
-        gzrite(STDERR, "Nu pot deschide sursa: $url\n");
-        return;
-    }
+$reader = new XMLReader();
+if (!$reader->open($source)) {
+    fwrite(STDERR, "Nu pot deschide sursa: $source\n");
+    exit(1);
+}
 
-    $now = time();
+$now = time();
 
-    while ($reader->read()) {
-        if ($reader->nodeType == XMLReader::ELEMENT) {
-            if ($reader->name == "channel") {
-                $id = strtolower($reader->getAttribute("id"));
-                if (in_array($id, $channels)) {
-                  
-                    gzwrite($out, $reader->readOuterXML() . "\n");
-                }
-            }
+while ($reader->read()) {
+    if ($reader->nodeType == XMLReader::ELEMENT) {
+        if ($reader->name == "channel") {
+            gzwrite($out, $reader->readOuterXML() . "\n");
+        }
 
-            if ($reader->name == "programme") {
-                $id = strtolower($reader->getAttribute("channel"));
-                if (in_array($id, $channels)) {
-                    $start = $reader->getAttribute("start");
-                    $stop  = $reader->getAttribute("stop");
-                    $stopTime = DateTime::createFromFormat("YmdHis O", $stop);
+        if ($reader->name == "programme") {
+            $start = $reader->getAttribute("start");
+            $stop  = $reader->getAttribute("stop");
+            $channel = $reader->getAttribute("channel");
 
-                    if ($stopTime && $stopTime->getTimestamp() >= $now) {
-                        $xml = new SimpleXMLElement($reader->readOuterXML());
-                        $title = htmlspecialchars((string)$xml->title, ENT_XML1 | ENT_QUOTES, 'UTF-8');
+            $startTime = DateTime::createFromFormat("YmdHis O", $start);
+            $stopTime  = DateTime::createFromFormat("YmdHis O", $stop);
 
-                    
-                        gzwrite($out, "<programme channel=\"$id\" start=\"$start\" stop=\"$stop\">\n");
-                        gzwrite($out, "  <title>$title</title>\n");
-                        gzwrite($out, "</programme>\n");
-                    }
+            if ($startTime && $stopTime) {
+                // conversie la fusul orar dorit
+                $startTime->setTimezone($targetTZ);
+                $stopTime->setTimezone($targetTZ);
+
+                if ($stopTime->getTimestamp() >= $now) {
+                    $xml = new SimpleXMLElement($reader->readOuterXML());
+                    $title = htmlspecialchars((string)$xml->title, ENT_XML1 | ENT_QUOTES, 'UTF-8');
+
+                    gzwrite($out, "<programme channel=\"$channel\" start=\"".$startTime->format("YmdHis O")."\" stop=\"".$stopTime->format("YmdHis O")."\">\n");
+                    gzwrite($out, "  <title>$title</title>\n");
+                    gzwrite($out, "</programme>\n");
                 }
             }
         }
     }
-    $reader->close();
 }
+$reader->close();
 
-foreach ($sources as $src) {
-    fetchEPG($src, $channels, $out);
-}
-;
 gzwrite($out, "</tv>\n");
 gzclose($out);
 
-echo "EPG comprimat generat cu succes.\n";
+echo "EPG ajustat la fus orar Europe/Chisinau generat cu succes.\n";
